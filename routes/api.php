c0690b7768a134edd9fb3d20574065e6f90dc214
dasaint001 <elijahbamidele48@yahoo.com>
2022-10-27 01:21:23 +0100

    route changed

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get( '/', [DataController::class, 'getMyInfo'] );
+Route::get( '/my-info', [DataController::class, 'getMyInfo'] );
 
